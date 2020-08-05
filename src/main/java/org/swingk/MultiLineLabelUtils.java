6961185f1ba3222e740291594c67a7bce1420425
Eli Rosenblum <parubok4@gmail.com>
2020-08-05 21:55:12 +0300

    Updates.

--- src/main/java/org/swingk/MultiLineLabelUtils.java
+++ src/main/java/org/swingk/MultiLineLabelUtils.java
@@ -46,9 +46,24 @@ class MultiLineLabelUtils {
     }
 
     static class NextLine {
+        /**
+         * True if this is the last line of the text (end of text).
+         */
         public final boolean lastLine;
+
+        /**
+         * Index of first character in the line. Inclusive.
+         */
         public final int lineStartIndex;
+
+        /**
+         * Index of last character in the line. Inclusive.
+         */
         public final int lineEndIndex;
+
+        /**
+         * Index of first character in the line after that line. Inclusive.
+         */
         public final int nextLineStartIndex;
 
         NextLine(boolean lastLine,
