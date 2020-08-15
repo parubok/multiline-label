e52c1c857e809b166cef8d1fdcf6c9b1a4e619d5
Eli Rosenblum <parubok4@gmail.com>
2020-08-15 13:35:34 +0300

    Updates.

--- src/test/java/org/swingk/DemoScrollPane.java
+++ src/test/java/org/swingk/DemoScrollPane.java
@@ -4,23 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
-import javax.swing.border.EmptyBorder;
 import java.awt.BorderLayout;
 
-public class Demo2 {
+public class DemoScrollPane {
     public static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed " +
             "bibendum, lacus vel convallis consectetur, erat dui pharetra lectus, ac venenatis nulla nisi eget erat. " +
             "Donec ornare volutpat augue, a venenatis magna rutrum non.";
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(Demo2::buildUI);
+        SwingUtilities.invokeLater(DemoScrollPane::buildUI);
     }
 
     private static void buildUI() {
         JPanel contentPanel = new JPanel(new BorderLayout());
 
         MultiLineLabel label = new MultiLineLabel();
-        label.setBorder(new EmptyBorder(3, 3, 3, 3));
         label.setText(LOREM_IPSUM + " " + LOREM_IPSUM + " " + LOREM_IPSUM);
 
         JScrollPane sp = new JScrollPane();
