2460b977df49f3782d406be18a9dd806904521fd
Eli Rosenblum <parubok4@gmail.com>
2020-08-17 07:18:28 +0300

    More unit tests.

--- src/test/java/org/swingk/MultiLineLabelUtilsTest.java
+++ src/test/java/org/swingk/MultiLineLabelUtilsTest.java
@@ -104,4 +104,54 @@ class MultiLineLabelUtilsTest {
             Assertions.assertEquals(-1, nextLine2.nextLineStartIndex);
         });
     }
+
+    @Test
+    void getNextLine_7() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            String text = "a abcdvfsl2808kdfkfkdjk94893483dkjdkfjkfjkdjfkdfjdkjdkd abcdvfsl2808kdfkfkdjk94893483dkjdkfjkfjkdjfkdfjdkjdkd";
+            MultiLineLabel label = new MultiLineLabel();
+            FontMetrics fm = label.getFontMetrics(font);
+            final int limit = 30;
+            MultiLineLabelUtils.NextLine nextLine1 = MultiLineLabelUtils.getNextLine(text, 0, fm, limit);
+            Assertions.assertFalse(nextLine1.lastLine);
+            Assertions.assertEquals(0, nextLine1.lineStartIndex);
+            Assertions.assertEquals(0, nextLine1.lineEndIndex);
+            Assertions.assertEquals(2, nextLine1.nextLineStartIndex);
+            MultiLineLabelUtils.NextLine nextLine2 = MultiLineLabelUtils.getNextLine(text, nextLine1.nextLineStartIndex, fm, limit);
+            Assertions.assertFalse(nextLine2.lastLine);
+            Assertions.assertEquals(2, nextLine2.lineStartIndex);
+            Assertions.assertEquals(54, nextLine2.lineEndIndex);
+            Assertions.assertEquals(56, nextLine2.nextLineStartIndex);
+            MultiLineLabelUtils.NextLine nextLine3 = MultiLineLabelUtils.getNextLine(text, nextLine2.nextLineStartIndex, fm, limit);
+            Assertions.assertTrue(nextLine3.lastLine);
+            Assertions.assertEquals(56, nextLine3.lineStartIndex);
+            Assertions.assertEquals(108, nextLine3.lineEndIndex);
+            Assertions.assertEquals(-1, nextLine3.nextLineStartIndex);
+        });
+    }
+
+    @Test
+    void getNextLine_8() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            String text = "a abcdvfsl2808kdfkfkdjk94893483dkjdkfjkfjkdjfkdfjdkjdkd b";
+            MultiLineLabel label = new MultiLineLabel();
+            FontMetrics fm = label.getFontMetrics(font);
+            final int limit = 30;
+            MultiLineLabelUtils.NextLine nextLine1 = MultiLineLabelUtils.getNextLine(text, 0, fm, limit);
+            Assertions.assertFalse(nextLine1.lastLine);
+            Assertions.assertEquals(0, nextLine1.lineStartIndex);
+            Assertions.assertEquals(0, nextLine1.lineEndIndex);
+            Assertions.assertEquals(2, nextLine1.nextLineStartIndex);
+            MultiLineLabelUtils.NextLine nextLine2 = MultiLineLabelUtils.getNextLine(text, nextLine1.nextLineStartIndex, fm, limit);
+            Assertions.assertFalse(nextLine2.lastLine);
+            Assertions.assertEquals(2, nextLine2.lineStartIndex);
+            Assertions.assertEquals(54, nextLine2.lineEndIndex);
+            Assertions.assertEquals(56, nextLine2.nextLineStartIndex);
+            MultiLineLabelUtils.NextLine nextLine3 = MultiLineLabelUtils.getNextLine(text, nextLine2.nextLineStartIndex, fm, limit);
+            Assertions.assertTrue(nextLine3.lastLine);
+            Assertions.assertEquals(56, nextLine3.lineStartIndex);
+            Assertions.assertEquals(56, nextLine3.lineEndIndex);
+            Assertions.assertEquals(-1, nextLine3.nextLineStartIndex);
+        });
+    }
 }
