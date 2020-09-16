4a000dcd6e7e6c314aafce86ab396cc57d5428b1
Eli Rosenblum <parubok4@gmail.com>
2020-09-16 18:31:24 +0300

    Stub of ProvidedTextLayout.

---
+++ src/main/java/org/swingk/ProvidedTextLayout.java
@@ -0,0 +1,21 @@
+package org.swingk;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class ProvidedTextLayout implements TextLayout {
+    @Override
+    public void paintText(Graphics g) {
+        
+    }
+
+    @Override
+    public Dimension calculatePreferredSize() {
+        return null;
+    }
+
+    @Override
+    public void preSetBounds(int x, int y, int width, int height) {
+
+    }
+}
