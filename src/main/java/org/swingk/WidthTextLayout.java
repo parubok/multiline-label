a016ca9555c82ce3916d3c27770cbbff91beaf48
Eli Rosenblum <parubok4@gmail.com>
2020-09-25 17:01:26 +0300

    Refactoring.

--- src/main/java/org/swingk/WidthTextLayout.java
+++ src/main/java/org/swingk/WidthTextLayout.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Insets;
 import java.util.Objects;
 
+import static javax.swing.SwingUtilities.computeStringWidth;
 import static org.swingk.MultilineLabelUtils.paintTextInDisabledStyle;
 
 /**
@@ -15,7 +16,129 @@ import static org.swingk.MultilineLabelUtils.paintTextInDisabledStyle;
  */
 public class WidthTextLayout implements TextLayout {
 
-    protected static String toRenderedText(String text) {
+    /**
+     * @param insets Component insets. Not null.
+     * @param fm     Component {@link FontMetrics}. Not null.
+     * @param text   Text to paint. Not null.
+     * @param wLimit Requested component width limit in pixels. Greater than 0.
+     * @return Component preferred size.
+     */
+    static Dimension calcComponentPreferredSizeForWidthLimit(Insets insets, FontMetrics fm, String text,
+                                                                    int wLimit) {
+        assert insets != null;
+        assert fm != null;
+        assert text != null;
+        assert wLimit > 0;
+
+        final int horInsets = insets.right + insets.left;
+        int textPrefWidth;
+        int textPrefHeight;
+        if (!text.isEmpty()) {
+            assert fm != null;
+            NextLine nextLine;
+            int startIndex = 0;
+            final int textWidthLimit = Math.max(1, wLimit - horInsets);
+            int lineCount = 0;
+            int maxLineWidth = 0; // pixels
+            do {
+                nextLine = getNextLine(text, startIndex, fm, textWidthLimit);
+                String nextLineStr = text.substring(nextLine.lineStartIndex, nextLine.lineEndIndex + 1);
+                int nextLineWidth = computeStringWidth(fm, nextLineStr);
+                maxLineWidth = Math.max(maxLineWidth, nextLineWidth);
+                lineCount++;
+                startIndex = nextLine.nextLineStartIndex;
+            } while (!nextLine.lastLine);
+            textPrefWidth = maxLineWidth;
+            textPrefHeight = (fm.getAscent() + fm.getDescent()) * lineCount + fm.getLeading() * (lineCount - 1);
+        } else {
+            textPrefWidth = textPrefHeight = 0;
+        }
+        return new Dimension(textPrefWidth + horInsets, textPrefHeight + insets.top + insets.bottom);
+    }
+
+    /**
+     * @param text       Text to display in {@link MultilineLabel}.
+     * @param startIndex Index of 1st character in the new line.
+     * @param fm         Current {@link FontMetrics}.
+     * @param widthLimit Limit on the width of the line.
+     * @return Object with details of the next line.
+     */
+    static NextLine getNextLine(final String text, final int startIndex, final FontMetrics fm, final int widthLimit) {
+        assert text != null;
+        assert text.length() > 0;
+        assert startIndex > -1;
+        assert fm != null;
+        assert widthLimit > 0;
+        int spaceIndex = startIndex;
+        while (true) {
+            int nextSpaceIndex = text.indexOf(' ', spaceIndex + 1);
+            if (nextSpaceIndex == -1) { // there is no next space after spaceIndex
+                if (spaceIndex > startIndex && computeStringWidth(fm, text.substring(startIndex)) > widthLimit) {
+                    // next line will be single word last line
+                    return new NextLine(false, startIndex, spaceIndex - 1, spaceIndex + 1);
+                } else {
+                    // last line
+                    return new NextLine(true, startIndex, text.length() - 1, -1);
+                }
+            } else { // there is next space after spaceIndex
+                if (computeStringWidth(fm, text.substring(startIndex, nextSpaceIndex)) > widthLimit) {
+                    if (spaceIndex > startIndex) {
+                        // regular next line
+                        return new NextLine(false, startIndex, spaceIndex - 1, spaceIndex + 1);
+                    } else {
+                        // single word line
+                        return new NextLine(false, startIndex, nextSpaceIndex - 1, nextSpaceIndex + 1);
+                    }
+                } else {
+                    spaceIndex = nextSpaceIndex; // continue with current line
+                }
+            }
+        }
+    }
+
+    static class NextLine {
+        /**
+         * True if this is the last line of the text (end of text).
+         */
+        public final boolean lastLine;
+
+        /**
+         * Index of first character in the line. Inclusive.
+         */
+        public final int lineStartIndex;
+
+        /**
+         * Index of last character in the line. Inclusive.
+         */
+        public final int lineEndIndex;
+
+        /**
+         * Index of first character in the line after that line. Inclusive.
+         */
+        public final int nextLineStartIndex;
+
+        NextLine(boolean lastLine,
+                 int lineStartIndex,
+                 int lineEndIndex,
+                 int nextLineStartIndex) {
+            this.lastLine = lastLine;
+            this.lineStartIndex = lineStartIndex;
+            this.lineEndIndex = lineEndIndex;
+            this.nextLineStartIndex = nextLineStartIndex;
+        }
+
+        @Override
+        public String toString() {
+            return "NextLine{" +
+                    "lastLine=" + lastLine +
+                    ", lineStartIndex=" + lineStartIndex +
+                    ", lineEndIndex=" + lineEndIndex +
+                    ", nextLineStartIndex=" + nextLineStartIndex +
+                    '}';
+        }
+    }
+
+    static String toRenderedText(String text) {
         StringBuilder sb = new StringBuilder(text.replace('\n', ' ').trim());
         int doubleSpaceIndex;
         while ((doubleSpaceIndex = sb.indexOf("  ")) > -1) {
@@ -55,7 +178,7 @@ public class WidthTextLayout implements TextLayout {
         }
     }
 
-    protected Dimension calcPreferredSize(int expectedLabelWidth) {
+    private Dimension calcPreferredSize(int expectedLabelWidth) {
         final int wLimit;
         if (expectedLabelWidth > 0) {
             wLimit = expectedLabelWidth;
@@ -67,7 +190,7 @@ public class WidthTextLayout implements TextLayout {
         }
         final FontMetrics fm = label.getFontMetrics(label.getFont());
         final Insets insets = label.getInsets();
-        return MultilineLabelUtils.calcComponentPreferredSizeForWidthLimit(insets, fm, textToRender, wLimit);
+        return calcComponentPreferredSizeForWidthLimit(insets, fm, textToRender, wLimit);
     }
 
     @Override
@@ -84,10 +207,10 @@ public class WidthTextLayout implements TextLayout {
         final int x = insets.left;
         int y = insets.top + fm.getAscent();
         final boolean enabled = label.isEnabled();
-        MultilineLabelUtils.NextLine nextLine;
+        NextLine nextLine;
         int index = 0;
         do {
-            nextLine = MultilineLabelUtils.getNextLine(textToRender, index, fm, widthLimit);
+            nextLine = getNextLine(textToRender, index, fm, widthLimit);
             String lineStr = textToRender.substring(nextLine.lineStartIndex, nextLine.lineEndIndex + 1);
             if (enabled) {
                 g.drawString(lineStr, x, y);
