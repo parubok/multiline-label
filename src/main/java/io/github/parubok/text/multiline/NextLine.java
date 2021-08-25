7078f92a5aa8be7447befef296c3b8aa195e0440
parubok <parubok4@gmail.com>
2021-08-25 12:53:29 +0300

    Put class NextLine in its own file

---
+++ src/main/java/io/github/parubok/text/multiline/NextLine.java
@@ -0,0 +1,43 @@
+package io.github.parubok.text.multiline;
+
+final class NextLine {
+    /**
+     * True if this is the last line of the text (end of text).
+     */
+    public final boolean lastLine;
+
+    /**
+     * Index of first character in the line. Inclusive.
+     */
+    public final int lineStartIndex;
+
+    /**
+     * Index of last character in the line. Inclusive.
+     */
+    public final int lineEndIndex;
+
+    /**
+     * Index of first character in the line after that line. Inclusive.
+     */
+    public final int nextLineStartIndex;
+
+    NextLine(boolean lastLine,
+             int lineStartIndex,
+             int lineEndIndex,
+             int nextLineStartIndex) {
+        this.lastLine = lastLine;
+        this.lineStartIndex = lineStartIndex;
+        this.lineEndIndex = lineEndIndex;
+        this.nextLineStartIndex = nextLineStartIndex;
+    }
+
+    @Override
+    public String toString() {
+        return "NextLine{" +
+                "lastLine=" + lastLine +
+                ", lineStartIndex=" + lineStartIndex +
+                ", lineEndIndex=" + lineEndIndex +
+                ", nextLineStartIndex=" + nextLineStartIndex +
+                '}';
+    }
+}
--- src/main/java/io/github/parubok/text/multiline/WidthTextLayout.java
+++ src/main/java/io/github/parubok/text/multiline/WidthTextLayout.java
@@ -126,48 +126,6 @@ final class WidthTextLayout extends AbstractTextLayout {
         }
     }
 
-    static class NextLine {
-        /**
-         * True if this is the last line of the text (end of text).
-         */
-        public final boolean lastLine;
-
-        /**
-         * Index of first character in the line. Inclusive.
-         */
-        public final int lineStartIndex;
-
-        /**
-         * Index of last character in the line. Inclusive.
-         */
-        public final int lineEndIndex;
-
-        /**
-         * Index of first character in the line after that line. Inclusive.
-         */
-        public final int nextLineStartIndex;
-
-        NextLine(boolean lastLine,
-                 int lineStartIndex,
-                 int lineEndIndex,
-                 int nextLineStartIndex) {
-            this.lastLine = lastLine;
-            this.lineStartIndex = lineStartIndex;
-            this.lineEndIndex = lineEndIndex;
-            this.nextLineStartIndex = nextLineStartIndex;
-        }
-
-        @Override
-        public String toString() {
-            return "NextLine{" +
-                    "lastLine=" + lastLine +
-                    ", lineStartIndex=" + lineStartIndex +
-                    ", lineEndIndex=" + lineEndIndex +
-                    ", nextLineStartIndex=" + nextLineStartIndex +
-                    '}';
-        }
-    }
-
     static String toRenderedText(String text) {
         var sb = new StringBuilder(text.replace('\n', ' ').trim());
         int doubleSpaceIndex;
@@ -189,7 +147,7 @@ final class WidthTextLayout extends AbstractTextLayout {
         return calcPreferredSize(0);
     }
 
-    protected void requestLayout() {
+    private void requestLayout() {
         SwingUtilities.invokeLater(() -> {
             label.revalidate();
             label.repaint();
