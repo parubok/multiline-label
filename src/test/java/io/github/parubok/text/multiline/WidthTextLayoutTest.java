205708234bdaf29d2d59f681b5ed5c7815e494b4
parubok <parubok4@gmail.com>
2023-07-22 06:56:45 +0100

    Additional tests

--- src/test/java/io/github/parubok/text/multiline/WidthTextLayoutTest.java
+++ src/test/java/io/github/parubok/text/multiline/WidthTextLayoutTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.swing.SwingUtilities;
 import java.awt.Font;
+import java.util.Set;
 
 public class WidthTextLayoutTest {
 
@@ -186,6 +187,70 @@ public class WidthTextLayoutTest {
             Assertions.assertEquals(56, nextLine3.lineStartIndex);
             Assertions.assertEquals(56, nextLine3.lineEndIndex);
             Assertions.assertEquals(-1, nextLine3.nextLineStartIndex);
+        });
+    }
+
+    /**
+     * Case where line breaks ('\n') are provided in the text and the text has no separators.
+     */
+    @Test
+    public void getNextLine_providedLineBreaksNoSep() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            var text = "l1\nl2\nl123";
+            var label = new MultilineLabel();
+            var fm = label.getFontMetrics(font);
+            int limit = 3;
+            Set<Character> sep = MultilineLabel.DEFAULT_SEPARATORS;
+            var nextLine1 = WidthTextLayout.getNextLine(label, text, 0, fm, limit, sep);
+            Assertions.assertFalse(nextLine1.lastLine);
+            Assertions.assertEquals(0, nextLine1.lineStartIndex);
+            Assertions.assertEquals(1, nextLine1.lineEndIndex);
+            Assertions.assertEquals(3, nextLine1.nextLineStartIndex);
+            var nextLine2 = WidthTextLayout.getNextLine(label, text, nextLine1.nextLineStartIndex, fm, limit, sep);
+            Assertions.assertFalse(nextLine2.lastLine);
+            Assertions.assertEquals(3, nextLine2.lineStartIndex);
+            Assertions.assertEquals(4, nextLine2.lineEndIndex);
+            Assertions.assertEquals(6, nextLine2.nextLineStartIndex);
+            var nextLine3 = WidthTextLayout.getNextLine(label, text, nextLine2.nextLineStartIndex, fm, limit, sep);
+            Assertions.assertTrue(nextLine3.lastLine);
+            Assertions.assertEquals(6, nextLine3.lineStartIndex);
+            Assertions.assertEquals(9, nextLine3.lineEndIndex);
+            Assertions.assertEquals(-1, nextLine3.nextLineStartIndex);
+        });
+    }
+
+    /**
+     * Case where line breaks ('\n') are provided in the text and there is 1 separator ('a') in the last line.
+     */
+    @Test
+    public void getNextLine_providedLineBreaksWithSepInTheLastLine() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            var text = "l1\nl2\nl12a3";
+            var label = new MultilineLabel();
+            Set<Character> sep = Set.of('a');
+            label.setSeparators(sep);
+            var fm = label.getFontMetrics(font);
+            final int limit = 3;
+            var nextLine1 = WidthTextLayout.getNextLine(label, text, 0, fm, limit, sep);
+            Assertions.assertFalse(nextLine1.lastLine);
+            Assertions.assertEquals(0, nextLine1.lineStartIndex);
+            Assertions.assertEquals(1, nextLine1.lineEndIndex);
+            Assertions.assertEquals(3, nextLine1.nextLineStartIndex);
+            var nextLine2 = WidthTextLayout.getNextLine(label, text, nextLine1.nextLineStartIndex, fm, limit, sep);
+            Assertions.assertFalse(nextLine2.lastLine);
+            Assertions.assertEquals(3, nextLine2.lineStartIndex);
+            Assertions.assertEquals(4, nextLine2.lineEndIndex);
+            Assertions.assertEquals(6, nextLine2.nextLineStartIndex);
+            var nextLine3 = WidthTextLayout.getNextLine(label, text, nextLine2.nextLineStartIndex, fm, limit, sep);
+            Assertions.assertFalse(nextLine3.lastLine);
+            Assertions.assertEquals(6, nextLine3.lineStartIndex);
+            Assertions.assertEquals(9, nextLine3.lineEndIndex);
+            Assertions.assertEquals(10, nextLine3.nextLineStartIndex);
+            var nextLine4 = WidthTextLayout.getNextLine(label, text, nextLine3.nextLineStartIndex, fm, limit, sep);
+            Assertions.assertTrue(nextLine4.lastLine);
+            Assertions.assertEquals(10, nextLine4.lineStartIndex);
+            Assertions.assertEquals(10, nextLine4.lineEndIndex);
+            Assertions.assertEquals(-1, nextLine4.nextLineStartIndex);
         });
     }
 }
